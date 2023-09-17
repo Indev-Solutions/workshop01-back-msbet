58178ae126a0a19a3bee1ff5e2b0dd2830403a33
Jose Mori <joosem@gmail.com>
2023-09-16 22:13:52 -0500

    AP-14 create bet spring boot project

---
+++ src/main/java/com/indevsolutions/workshop/bet/BetApplication.java
@@ -0,0 +1,12 @@
+package com.indevsolutions.workshop.bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetApplication.class, args);
+	}
+}
