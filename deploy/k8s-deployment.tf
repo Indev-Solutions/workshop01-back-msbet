228b98202156fe9332ac2290229f694b0b7d763f
Christian Nuñez del Prado Rodriguez <cnunezdp@indevsolutions.com>
2024-03-31 14:33:53 +0200

    AP-35 refactoring was done for pipeline of continuous integration adding github actions of terraform

--- deploy/k8s-deployment.tf
+++ deploy/k8s-deployment.tf
@@ -2,7 +2,7 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "my_namespace" {
   metadata {
     name = "my-first-namespace"
   }
