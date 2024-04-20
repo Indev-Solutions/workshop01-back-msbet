c6c3f9d773ec82c1ee18f891af51d849db521697
Christian Nuñez del Prado Rodriguez <cnunezdp@indevsolutions.com>
2024-04-20 19:51:03 +0200

    AP-35 refactoring was done for pipeline of continuous integration adding new github actions of terraform

--- deploy/k8s-deployment.tf
+++ deploy/k8s-deployment.tf
@@ -2,8 +2,79 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_namespace" "my_namespace" {
+variable "DATABASE_HOSTNAME" {
+  type        = string
+  description = "Hostname of database"
+  sensitive   = true
+}
+
+variable "DATABASE_PASSWORD" {
+  type        = string
+  description = "Password of database"
+  sensitive   = true
+}
+
+resource "kubernetes_deployment" "msbet-deployment" {
   metadata {
-    name = "my-first-namespace"
+    name = "workshop1-deployment-msbet"
+    labels = {
+      app = "workshop1-backend-msbet"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "workshop1-backend-msbet"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "workshop1-backend-msbet"
+        }
+      }
+
+      spec {
+        container {
+          image = "indevsolutions/workshop1:ms-bet_v5"
+          name  = "customservicebet"
+
+          port = [{
+            name = "tcp"
+            container_port = 8080
+          }]
+
+          env {
+            name = "DATABASE_URL"
+            value = "jdbc:postgresql://${var.DATABASE_HOSTNAME}:5432/workshop"
+          }
+
+          env {
+            name = "DATABASE_USER"
+            value = "dbadmin"
+          }
+
+          env {
+            name = "DATABASE_PASSWORD"
+            value = var.DATABASE_PASSWORD
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
   }
 }
